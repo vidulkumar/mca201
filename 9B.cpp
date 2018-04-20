131dc05a6818b02a1cc8988f2c9aae3e0a811da9
Vidul <vidulkumar@gmail.com>
2018-04-20 12:06:34 +0530

    Add files via upload

--- 9B.cpp
+++ 9B.cpp
@@ -18,15 +18,15 @@ void readrec()
       cerr<<"file can't be opened";
     }
     //in.read((char*)&x,sizeof(Employee));
-
-    in.read((char*)&x.EmpNo,sizeof(x.EmpNo));
-    //out<<"|";
-    in.read((char*)&x.EmpName,sizeof(x.EmpName));
-    //out<<"|";
-    in.read((char*)&x.Department,sizeof(x.Department));
-    //out<<"\n";
-   cout<<x.EmpNo<<" "<<x.EmpName<<" "<<x.Department;
-}
+    cout<<"empno|           |empname|        |department|\n";
+    in.read((char*)&x,sizeof(x));
+    while(!in.eof())
+    {
+	
+   cout<<x.EmpNo<<" "<<x.EmpName<<" "<<x.Department<<"\n";
+   in.read((char*)&x,sizeof(x));
+    };
+};
 void writerec()
 {   Employee x;
     ofstream out("record.txt",ios::in|ios::app);
@@ -41,21 +41,37 @@ void writerec()
     cin>>x.EmpName;
     cout<<"\nenter employee Department : ";
     cin>>x.Department;
-    out.write((char*)&x.EmpNo,sizeof(x.EmpNo));
-    out<<" ";
-    out.write((char*)&x.EmpName,sizeof(x.EmpName));
-    out<<" ";
-    out.write((char*)&x.Department,sizeof(x.Department));
-    out<<" ";
+    out.write((char*)&x,sizeof(x));
+    //out<<" ";
+    //out.write((char*)&x.EmpName,sizeof(x.EmpName));
+    //out<<" ";
+    //out.write((char*)&x.Department,sizeof(x.Department));
+    //out<<" ";
     out.close();
 }
 
 int main()
-{
-  readrec();
-  writerec();
-  readrec();
-  writerec();
+{   int ch;
+   
+   do{
+   
+    cout<<"\n-----------menu--------------";
+    cout<<"\n1. write in to file";
+    cout<<"\n2. read all records from  file ";
+    cout<<"\n press 0 to exit ";
+    cout<<"\nenter your choice ";
+    cin>>ch;
+    switch(ch)
+    {
+
+    case 1: writerec();
+            break;
+    case 2: readrec();
+            break;
+    default : if(ch!=0)
+             cout<<"no such option";
+    };
+    }while(ch!=0);
 
 
 return 0;
